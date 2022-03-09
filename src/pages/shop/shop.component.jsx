d56ef918e914e3967aa6d447d879a289160a9615
sajid islam <anindasajid@gmail.com>
2022-03-09 16:22:55 +0600

    add to cart redux with quantity

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CollectionPreview from '../../components/preview-collection/preview-collection.component';
-import { SHOP_DATA } from './shop.data';
+import SHOP_DATA from './shop.data';
 
 export default class ShopPage extends Component {
   constructor(props) {
